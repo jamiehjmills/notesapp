9e74261a41ce7d504b11350fc0e2132338996fdd
Jamie Mills <hmills6@myune.edu.au>
2020-11-06 20:28:18 +1100

    creating a gradle project in IntelliJ and set up a client side to commence the project

---
+++ client/src/main/java/App.java
@@ -0,0 +1,31 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    private int appWidth = 600;
+    private int appHeight = 600;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        // Create and show the title screen
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("application.fxml"));
+        fxmlLoader.setLocation(getClass().getResource("application.fxml"));
+        Parent titleScreen = fxmlLoader.load();
+
+        // Display the title screen
+        Scene scene = new Scene(titleScreen, appWidth, appHeight);
+        stage.setTitle("Notes");
+        stage.setScene(scene);
+        stage.show();
+
+
+    }
+}
+
+
+
