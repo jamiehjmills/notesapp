4a7879ce0f2b250df95dc365b06fc7da534a1533
Jamie Mills <hmills6@myune.edu.au>
2020-11-21 17:06:43 +1100

    updating the note creation function

--- client/src/main/java/AppController.java
+++ client/src/main/java/AppController.java
@@ -1,29 +1,53 @@
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class AppController {
 
     @FXML
-    AnchorPane pane;
+    private AnchorPane root;
+    private int yPosition, xPosition  = 0; // x and y positions for the notes
+    private boolean postingNotes = true;
 
     @FXML
     public void creatingNotes(ActionEvent event){
 
         System.out.println("testing = ok");
-        notes(200,200);
+
+        double[] y = {93,262,432};
+        double[] x = {34, 224, 412};
+
+        if (postingNotes) {
+            notes(140,140,x[xPosition],y[yPosition]);
+            xPosition++;
+        };
+
+        if (xPosition >= 3){
+            yPosition++;
+            xPosition = 0;
+        }
+
+        if (yPosition >= 2 && xPosition >= 3 ){
+            postingNotes= false;
+            yPosition = 0;
+            xPosition = 0;
+
+        }
+
 
     }
 
-    public void notes(float width, float height){
+    public void notes(double width, double height, double x, double y){
 
         Rectangle r = new Rectangle();
-        r.setX(50);
-        r.setY(50);
-        r.setWidth(200);
-        r.setHeight(100);
+        r.setX(x);
+        r.setY(y);
+        r.setWidth(width);
+        r.setHeight(height);
+        r.setFill(Color.LIGHTGRAY);
+        root.getChildren().add(r);
 
     }
 
