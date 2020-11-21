68e30b410267b39b3ecd438d5de51edc12106d0c
Jamie Mills <hmills6@myune.edu.au>
2020-11-21 15:46:11 +1100

    creating observer java and updated its layout

--- client/src/main/java/AppController.java
+++ client/src/main/java/AppController.java
@@ -1,2 +1,30 @@
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Rectangle;
+
 public class AppController {
+
+    @FXML
+    AnchorPane pane;
+
+    @FXML
+    public void creatingNotes(ActionEvent event){
+
+        System.out.println("testing = ok");
+        notes(200,200);
+
+    }
+
+    public void notes(float width, float height){
+
+        Rectangle r = new Rectangle();
+        r.setX(50);
+        r.setY(50);
+        r.setWidth(200);
+        r.setHeight(100);
+
+    }
+
 }
---
+++ client/src/main/java/Notes.java
@@ -0,0 +1,9 @@
+import javafx.scene.shape.Rectangle;
+
+public class Notes implements Observer{
+
+    @Override
+    public void update(String message) {
+
+    }
+}
