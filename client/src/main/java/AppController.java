fbe012bc2e7d4b9239f54ead5cff1dbdb32fae76
Jamie Mills <hmills6@myune.edu.au>
2020-11-23 18:01:14 +1100

    creating note objects. and testing to make sure it works

--- client/src/main/java/AppController.java
+++ client/src/main/java/AppController.java
@@ -4,6 +4,9 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AppController {
 
     @FXML
@@ -11,6 +14,20 @@ public class AppController {
     private int yPosition, xPosition  = 0; // x and y positions for the notes
     private boolean postingNotes = true;
     private int box = 0; // this is only for testing
+    private List<Notes> notesList = new ArrayList<>();
+    private Notes notes;
+    private int listPostion = 0;
+
+    public void initialize(){
+
+       for(int i =0; i<9;i++){
+           notesList.add(i, new Notes());
+       }
+
+       System.out.println(notesList); //1. to add observers. It works fine!
+        //todo: creating remove observers by creating a new button.
+
+    }
 
     @FXML
     public void creatingNotes(ActionEvent event){
@@ -19,7 +36,7 @@ public class AppController {
         double[] x = {34, 224, 412};
 
         if (postingNotes) {
-            notes(140,140,x[xPosition],y[yPosition]);
+            notesList.get(listPostion).create(140,140,x[xPosition],y[yPosition], root);
             xPosition++;
             box++; // this is only for testing
             System.out.println("Number of note: "+ box); //total 9 notes are made after that it cannot create anymore
@@ -31,21 +48,24 @@ public class AppController {
         }else if (xPosition > 2 && yPosition >= 2){
             yPosition = 0;
             xPosition = 0;
+            listPostion = 0;
             postingNotes = false;
         }
+
+        listPostion++;
     }
 
-    public void notes(double width, double height, double x, double y){
-
-        Rectangle r = new Rectangle();
-        r.setX(x);
-        r.setY(y);
-        r.setWidth(width);
-        r.setHeight(height);
-        r.setFill(Color.LIGHTGRAY);
-        root.getChildren().add(r);
-
-    }
+//    public void notes(double width, double height, double x, double y){
+//
+//        Rectangle r = new Rectangle();
+//        r.setX(x);
+//        r.setY(y);
+//        r.setWidth(width);
+//        r.setHeight(height);
+//        r.setFill(Color.LIGHTGRAY);
+//        root.getChildren().add(r);
+//
+//    }
 }
 
 
