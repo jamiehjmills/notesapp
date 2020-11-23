44f2273539e87c0210a45a4555d36c8c694f47bb
Jamie Mills <hmills6@myune.edu.au>
2020-11-23 22:07:18 +1100

    added menu, menuitems and menubar

--- client/src/main/java/Notes.java
+++ client/src/main/java/Notes.java
@@ -1,3 +1,6 @@
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -13,8 +16,25 @@ public class Notes implements Observer{
         r.setWidth(width);
         r.setHeight(height);
         r.setFill(Color.LIGHTGRAY);
-        root.getChildren().add(r);
 
+        MenuBar menu = new MenuBar();
+        menu.setPrefSize(width,20);
+        menu.setLayoutX(x);
+        menu.setLayoutY(y);
+
+        Menu mainMenu = new Menu("...");
+
+        MenuItem menuItem1 = new MenuItem("Maximise");
+        MenuItem menuItem2 = new MenuItem("Close");
+
+        //add a main menu onto a menubar
+        menu.getMenus().add(mainMenu);
+
+        //add two menu item (drop-down list) into the menu
+        mainMenu.getItems().add(menuItem1);
+        mainMenu.getItems().add(menuItem2);
+
+        root.getChildren().addAll(r,menu);
     }
 
 //    @Override
